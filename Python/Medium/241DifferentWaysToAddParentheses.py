94bee9347c3e9bee917b27e2a03c89957db8e830
Rohit Rasam <rasamrohit@gmail.com>
2025-08-17 20:54:47 +0530

    241DifferentWaysToAddParentheses

---
+++ Python/Medium/241DifferentWaysToAddParentheses.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/different-ways-to-add-parentheses/description/
+
+
+from typing import List
+
+
+def diffWaysToCompute(expression: str) -> List[int]:
+    pass
+
+
+if __name__ == '__main__':
+
+    case = "2-1-1"
+    print(diffWaysToCompute(case))
+    case = "2*3-4*5"
+    print(diffWaysToCompute(case))
