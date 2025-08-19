4d450a1c10e6f692dc374e0a834bbbd6166760e2
Rohit Rasam <rasamrohit@gmail.com>
2025-08-19 10:10:30 +0530

    241DifferentWaysToAddParentheses

--- Python/Medium/241DifferentWaysToAddParentheses.py
+++ Python/Medium/241DifferentWaysToAddParentheses.py
@@ -5,11 +5,28 @@ from typing import List
 
 
 def diffWaysToCompute(expression: str) -> List[int]:
-    pass
+    ops = {"+": lambda x, y: x + y, "-": lambda x, y: x - y, "*": lambda x, y: x * y}
+
+    def bt(left: int, right: int) -> List[int]:
+        res = []
+
+        for i in range(left, right + 1):
+            op = expression[i]
+            if op in ops:
+                nums1 = bt(left, i - 1)
+                nums2 = bt(i + 1, right)
+                for n1 in nums1:
+                    for n2 in nums2:
+                        res.append(ops[op](n1, n2))
+
+        if res == []:
+            res.append(int(expression[left:right+1]))
+        return res
+
+    return bt(0, len(expression) - 1)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     case = "2-1-1"
     print(diffWaysToCompute(case))
     case = "2*3-4*5"
