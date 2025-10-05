10496fde90893596b7abef2e0b866d53c4a08792
Rohit Rasam <rasamrohit@gmail.com>
2025-10-05 15:10:25 +0530

    134GasStation

--- Python/Medium/134GasStation.py
+++ Python/Medium/134GasStation.py
@@ -4,13 +4,25 @@ from typing import List
 
 
 def canCompleteCircuit(gas: List[int], cost: List[int]) -> int:
-    pass
+    if sum(gas) < sum(cost):
+        return -1
+    
+    total = 0
+    res = 0
+    for i in range(len(gas)):
+        total += (gas[i] - cost[i])
+
+        if total < 0:
+            total = 0
+            res = i + 1
+
+    return res
 
 
-if __name__ == '__main__':
-
-
-    case = [1,2,3,4,5], [3,4,5,1,2]
+if __name__ == "__main__":
+    case = [1, 2, 3, 4, 5], [3, 4, 5, 1, 2]
     print(canCompleteCircuit(*case))
-    case = [2,3,4], [3,4,3]
+    case = [2, 3, 4], [3, 4, 3]
+    print(canCompleteCircuit(*case))
+    case = [1,6,3,4,5], [3,4,5,1,2]
     print(canCompleteCircuit(*case))
